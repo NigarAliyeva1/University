0e9c6de7729cba9a5bf3ef87535004c08e5b1663
NigarAliyeva1 <nigaraliyeva959@gmail.com>
2022-11-15 16:55:29 +0400

    Create new project

---
+++ MyProjectUniversity/University/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using University.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace University.DAL
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
